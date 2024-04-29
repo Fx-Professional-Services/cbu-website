71b64703f9f58e5bcd9526e18b7ab3b7f3dda0ca
Christine Fradejas <christine@vidaura.com.au>
2024-04-30 01:46:42 +0800

    feat: initial form for payment details page

--- src/app/overview/payment/page.js
+++ src/app/overview/payment/page.js
@@ -1,38 +1,165 @@
+
 export default function Payment() {
     return (
         <>
             <div className="grid min-h-full grid-cols-1 grid-rows-[1fr,auto,1fr] bg-white lg:grid-cols-[max(50%,36rem),1fr] p-[5%]">
-                {/* <header className="mx-auto w-full max-w-7xl px-6 pt-6 sm:pt-10 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:px-8">
-                    <a href="#">
-                        <span className="sr-only">Your Company</span>
-                        <img
-                            className="h-10 w-auto sm:h-12"
-                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                            alt=""
-                        />
-                    </a>
-                </header> */}
-                <main className="mx-auto w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
+                <main className="mx-auto w-full max-w-7xl px-6  lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
                     <div className="max-w-lg">
-                        {/* <p className="text-base font-semibold leading-8 text-indigo-600">404</p> */}
-                        <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl cbu-accent-text">Page still in Development</h1>
-                        <p className="mt-6 text-base leading-7 text-gray-600">
-                            Sorry, we&apos;re still building this page for you.
-                        </p>
-                        <div className="mt-10">
-                            <a href="/overview/planner" className="text-sm font-semibold leading-7 text-yellow-600">
-                                <span aria-hidden="true">&larr;</span> Back to home
-                            </a>
-                        </div>
+                        <form>
+                            <div className="space-y-12">
+                                <div className="border-b border-gray-900/10 pb-12">
+                                    <h2 className="text-3xl font-semibold leading-10 text-gray-900">Payment details</h2>
+
+                                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
+                                        <div className="sm:col-span-full">
+                                            <label htmlFor="nameOnCard" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Name on card
+                                            </label>
+                                            <div className="mt-2 flex justify-between space-x-4">
+                                                <div className="flex-1 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-600 sm:max-w-md">
+                                                    <input
+                                                        type="text"
+                                                        name="firstName"
+                                                        id="firstName"
+                                                        autoComplete="firstName"
+                                                        className="block border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                        placeholder="Jane"
+                                                    />
+                                                </div>
+
+                                                <div className="flex-1 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-600 sm:max-w-md">
+                                                    <input
+                                                        type="text"
+                                                        name="lastName"
+                                                        id="lastName"
+                                                        autoComplete="lastName"
+                                                        className="block flex-2 border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                        placeholder="Smith"
+                                                    />
+
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+
+                                        {/* card details */}
+                                        <div className="col-span-full">
+                                            <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Card details
+                                            </label>
+                                            <div className="mt-.5">
+                                                <div className="w-[100%] flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-600">
+                                                
+                                                    <input
+                                                        type="text"
+                                                        name="cardNumber"
+                                                        id="cardNumber"
+                                                        autoComplete="cardNumber"
+                                                        className="w-[50%] block border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                        placeholder="1234-1234-1234-1234"
+                                                    />
+                                                    <input
+                                                        type="text"
+                                                        name="cardDate"
+                                                        id="cardDate"
+                                                        autoComplete="cardDate"
+                                                        className="w-[25%] block border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                        placeholder="MM/YY"
+                                                    />
+
+                                                    <input
+                                                        type="text"
+                                                        name="cardCVV"
+                                                        id="cardCVV"
+                                                        autoComplete="cardCVV"
+                                                        className="w-[25%] block border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                        placeholder="CVV"
+                                                    />
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="col-span-full">
+                                            <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Billing address (optional)
+                                            </label>
+                                            <div className="mt-2">
+                                                <div className="w-[100%] rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-600">
+                                                
+                                                    <input
+                                                        type="text"
+                                                        name="billingAddress"
+                                                        id="billingAddress"
+                                                        autoComplete="billingAddress"
+                                                        className="w-[100%] block border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                        placeholder="Address"
+                                                    />
+
+                                                </div>
+                                            </div>
+
+                                            <div className=" mt-2 flex justify-between space-x-4">
+                                                <div className="w-[100%] flex-1 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-600">
+                                                
+                                                    <input
+                                                        type="text"
+                                                        name="billingCity"
+                                                        id="billingCity"
+                                                        autoComplete="billingCity"
+                                                        className="block border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                        placeholder="City"
+                                                    />
+
+                                                </div>
+                                                <div className="w-[100%] flex-1 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-600">
+                                                
+                                                    <input
+                                                        type="text"
+                                                        name="billingCity"
+                                                        id="billingCity"
+                                                        autoComplete="billingCity"
+                                                        className="block border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                        placeholder="State"
+                                                    />
+
+                                                </div>
+                                            </div>
+                                            <div className=" mt-2 flex justify-between space-x-4">
+                                                <div className="w-[100%] flex-1 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-600">
+                                                
+                                                    <input
+                                                        type="text"
+                                                        name="billingCity"
+                                                        id="billingCity"
+                                                        autoComplete="billingCity"
+                                                        className="block border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                        placeholder="Zip"
+                                                    />
+
+                                                </div>
+                                                <div className="w-[100%] flex-1 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-600">
+
+                                                    <select
+                                                    id="country"
+                                                    name="country"
+                                                    autoComplete="country-name"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-yellow-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                                                    >
+                                                    <option>United States</option>
+                                                    <option>Canada</option>
+                                                    <option>Mexico</option>
+                                                    </select>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </main>
-                <div className="hidden lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block">
-                    <img
-                        src="https://images.unsplash.com/photo-1470847355775-e0e3c35a9a2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1825&q=80"
-                        alt=""
-                        className="absolute inset-0 h-full w-full object-contain"
-                    />
-                </div>
             </div>
         </>
     )
