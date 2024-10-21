c3a82ad2aabfd443ab247464a07c039cf1c45321
Christine Fradejas <christine@vidaura.com.au>
2024-10-21 21:52:48 +0800

    fix: add sales order item

--- pages/api/adminAPI/addSalesOrderItem.js
+++ pages/api/adminAPI/addSalesOrderItem.js
@@ -2,11 +2,11 @@ const host = process.env.NEXT_PUBLIC_API_URL;
 const version = process.env.NEXT_PUBLIC_FM_API_VERSION;
 
 export default async function handler(req, res) {
-    const { token, orderID, configurationOptionItemID, customerTierID, itemID, parentOrderItemID = "", partyID, price, quantity, subtotal, withTax, tax, taxableAmount, isConfirmed, isInvoiced, isReturned, isShipped, discountAmount } = req.body;
+    const { token, orderID, configurationOptionItemID, customerTierID, itemID, parentOrderItemID = "", partyID} = req.body;
     const response = await fetch(
-      `${host}/fmi/data/${version}/databases/Horizon Order/layouts/query: sales order item v1/`,
+      `${host}/fmi/data/${version}/databases/Horizon Order/layouts/query: sales order item v1/records`,
       {
-        method: "PATCH",
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
@@ -16,20 +16,9 @@ export default async function handler(req, res) {
           "_configuration option item id": configurationOptionItemID,
           "_customer tier id": customerTierID,
           "_item id": itemID,
-          "_order id": "C1E4B977-62A8-B54D-B32B-B9F1EDAC1045",
+          "_order id": orderID,
           "_parent order item id": parentOrderItemID,
-          "_party id": partyID,
-          "price": price,
-          "quantity": quantity,
-          "subtotal": subtotal,
-          "subtotal with tax": withTax,
-          "tax": tax,
-          "taxable amount": taxableAmount,
-          "is confirmed": isConfirmed,
-          "is invoiced": isInvoiced,
-          "is returned": isReturned,
-          "is shipped": isShipped,
-          "discount amount": discountAmount
+          "_party id": partyID
           }
       }),
       }
